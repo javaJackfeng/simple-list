4a29715ed9409a397ca96cfc9d9c16ac3790f40f
fengyb <fengyb@dazhuanjia.com>
2021-06-08 15:29:24 +0800

    feat: init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,65 @@
-import logo from './logo.svg';
+import { useReducer } from 'react'
+import { Waypoint } from 'react-waypoint'
 import './App.css';
 
+const mockArray = new Array(10);
+
+const mockList = {
+    isLastPage: false,
+    loading: false,
+    error: {
+        code: 101,
+        message: '',
+    },
+    items: mockArray.fill(1),
+}
+
+const Line = (props) => {
+    const {index, name} = props
+    return <div className="line">{index} -- {name}</div>
+}
+
+const listReducer = (state, payload) => {
+    const { items: preItems } = state || {}
+    const { items: curItems } = payload || {}
+    let newItems = [...preItems]
+    if (curItems) {
+        newItems  = [...preItems, ...curItems]
+    }
+    return {...state, ...payload, ...{ items: newItems }}
+}
+
 function App() {
+  const [list, dispatch] = useReducer(listReducer, mockList)
+  const safeItems = list.items
+  const { loading, isLastPage } = list
+  const loadData = () => {
+      const { items } = list
+      dispatch({loading: true})
+      setTimeout(() => {
+        if (items.length < 100) {
+            dispatch({items: [1,2,3,4,5], loading: false })
+          } else {
+            dispatch({isLastPage: true, loading: false })
+        }
+      }, 500)
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+        {safeItems.map((ele, index) => {
+            return <Line index={index} name={ele} />
+        })}
+        <div
+            style={{
+                textAlign: 'center',
+                height: '1px',
+                width: '100%',
+                clear: 'both'
+            }}
         >
-          Learn React
-        </a>
-      </header>
+            {loading && <span>加载中</span>}
+            {!isLastPage && <Waypoint onEnter={loadData} threshold={0.1} />}
+        </div>
     </div>
   );
 }
